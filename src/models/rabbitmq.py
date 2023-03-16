2b263fc8dd8998e8244ea89a09bc459e63637ec1
Promise Fru <promisefru1@gmail.com>
2023-03-16 15:53:19 +0100

    chg: dev: updated rabbitmq ssl context

--- src/models/rabbitmq.py
+++ src/models/rabbitmq.py
@@ -194,10 +194,7 @@ class RabbitMQModel:
             credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_password)
 
             if rabbitmq_ssl_active:
-                print("cert files ...............")
-                print(rabbitmq_ssl_cacert, rabbitmq_ssl_crt, rabbitmq_ssl_key)
-                context = ssl.create_default_context(cafile=rabbitmq_ssl_cacert)
-                context.load_cert_chain(rabbitmq_ssl_crt, rabbitmq_ssl_key)
+                context = ssl.create_default_context()
 
                 ssl_options = pika.SSLOptions(context)
                 conn_params = pika.ConnectionParameters(
