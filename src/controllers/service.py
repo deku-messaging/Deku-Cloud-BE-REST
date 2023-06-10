820b5bb8639390d0929fe01b2d29969cb453749f
Promise Fru <promisefru1@gmail.com>
2023-06-10 23:45:49 +0100

    new: dev: Added github workflow

--- src/controllers/service.py
+++ src/controllers/service.py
@@ -232,7 +232,7 @@ def publish_with_deku_client(
 
     rabbitmq.publish_to_exchange(
         body=body,
-        routing_key=service_name,
+        routing_key=service_name.replace("_", "."),
         exchange=project_reference,
         virtual_host=user.get("account_sid"),
     )
